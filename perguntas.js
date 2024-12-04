e6d2f3206118de05d92c05c2047424bc41d863dd
Prof-Pamela <pahh.alvarez33@gmail.com>
2024-12-04 14:28:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'o que é, o que é ?',
+    'Está sempre na sua frente, mas você nunca consegue ver?',
+    'O Futuro'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Você sabia que, se pudesse viajar no tempo, poderia usar conceitos de física para viajar para o futuro de maneira mais rápida que para o passado? Como você acha que isso funcionaria e o que seria necessário para que isso acontecesse?',
+    'Essa questão está relacionada à teoria da relatividade de Einstein, especialmente à dilatação do tempo. De acordo com essa teoria, o tempo não é absoluto e pode ser afetado pela velocidade de um objeto ou pela presença de um campo gravitacional forte.'
+)
+
+criaCartao(
+    'Meio Ambiente',
+    'Você sabia que as árvores podem comunicar-se entre si por meio de suas raízes? Como você acha que isso pode ajudar as florestas a se manterem saudáveis?',
+    'As árvores podem se comunicar entre si por meio de suas raízes através de uma rede subterrânea chamada mycorriza, que é formada por fungos que conectam as raízes das plantas.'
+)
+
+criaCartao(
+    'Engenharia Civil',
+    'Como a engenharia civil ajuda a melhorar a infraestrutura das cidades?',
+    'A engenharia civil projeta e constrói estruturas como pontes, estradas e edifícios, garantindo que sejam seguras, eficientes e sustentáveis para o uso diário da população.'
+)
